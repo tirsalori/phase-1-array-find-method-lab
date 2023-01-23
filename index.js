5a4fd7a728763cde71a2f1da7fd28577be607ce3
Tirsa St. Fort <lifelovelori@gmail.com>
2023-01-23 12:46:41 -0800

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+
+function superbowlWin(array){
+    let result = array.find(record => record.result === "W")
+    if (result !== undefined){
+        return result.year
+    }
+}
+
